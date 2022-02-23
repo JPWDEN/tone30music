462ab8452183e3704a1a6b1b56690669cb987524
Josh Walker <jpwhsv@gmail.com>
2022-02-23 11:57:27 -0700

    Initial commit

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Josh - vCard & Resume Template'"; //letter subject
+	$to = 'jpwhsv@gmail.com'; // Enter Your E-mail
+	$from = 'jpwhsv@gmail.com'; // Admin e-mail
+	$fromName = 'Josh Walker'; // Your company name
+	$charset = 'UTF-8';
+?>
